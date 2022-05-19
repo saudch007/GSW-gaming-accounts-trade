7909223e9d51c83ba819641c48015458dd97da37
Saud <f191001@nu.edu.pk>
2022-05-20 01:05:53 +0500

    Comitted by Saud - Forgot/Reset password functionality

---
+++ middlewares/auth.js
@@ -0,0 +1,24 @@
+const User = require("../models/User.model");
+
+
+function checkNotAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return res.redirect("/");
+  }
+  next();
+}
+
+function checkAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect("/login");
+}
+
+
+
+module.exports = {
+  checkNotAuthenticated,
+  checkAuthenticated,
+
+};
